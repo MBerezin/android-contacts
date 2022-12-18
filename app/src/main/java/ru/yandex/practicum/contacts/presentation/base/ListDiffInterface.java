8537a0c1b651036b70ae31f2c98b1bcdabeafb02
Maxim Berezin <beresin.m@googlemail.com>
2022-12-18 16:25:02 +0300

    add methods theSameAs and equals

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -1,4 +1,9 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
-public interface ListDiffInterface {
+public interface ListDiffInterface<T> {
+
+    boolean theSameAs(T t);
+
+    boolean equals(Object o);
+
 }
