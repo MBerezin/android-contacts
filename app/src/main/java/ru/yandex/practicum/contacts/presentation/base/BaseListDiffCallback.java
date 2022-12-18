a26e2a3b41e18faf9f7bb237751a7e50e8606f5c
Maxim Berezin <beresin.m@googlemail.com>
2022-12-18 16:56:24 +0300

    override methods areItemsTheSame, areContentsTheSame and getChangePayload

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -1,4 +1,27 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
-public class BaseListDiffCallback {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.DiffUtil;
+
+import ru.yandex.practicum.contacts.presentation.main.ContactUi;
+import ru.yandex.practicum.contacts.presentation.sort.SortTypeUI;
+
+public class BaseListDiffCallback<T extends ListDiffInterface> extends DiffUtil.ItemCallback<T>  {
+
+    @Override
+    public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
+        return oldItem.theSameAs(newItem);
+    }
+
+    @Override
+    public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
+        return oldItem.equals(newItem);
+    }
+
+    @Nullable
+    @Override
+    public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
+        return newItem;
+    }
 }
